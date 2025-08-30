2a6f281cd2ea762d44b47c4b88f4cee813db37dd
villarzz <hugovilar04@gmail.com>
2025-08-30 19:07:50 -0300

    feat: add width prop to HeaderButton component for customizable button size

--- validator/src/components/HeaderButtons.jsx
+++ validator/src/components/HeaderButtons.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./HeaderButtons.css";
 
-function HeaderButton({ ButtonText, ButtonIcon }) {
+function HeaderButton({ ButtonText, ButtonIcon, Width }) {
   return (
     <a className="header-button" href="#">
-      <img src={ButtonIcon} alt="" />
+      <img src={ButtonIcon} alt=""  width={Width}/>
       <p>{ButtonText}</p>
     </a>
   );
