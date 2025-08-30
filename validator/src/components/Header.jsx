36a9d5987fa8eab0f5a72108dbf3087a3f40c3e6
villarzz <hugovilar04@gmail.com>
2025-08-30 19:07:45 -0300

    refactor: reorganize Header component structure and improve readability

--- validator/src/components/Header.jsx
+++ validator/src/components/Header.jsx
@@ -1,23 +1,37 @@
-import React from "react";
 import "./Header.css";
-import Logo from "../assets/logoValidator.svg";
 import HomeIcon from "../assets/home.svg";
+import HeaderButton from "./HeaderButtons";
+import Logo from "../assets/logoValidator.svg";
 import TicketIcon from "../assets/ticket-alt.svg";
 import CardIcon from "../assets/address-card.svg";
-import HeaderButton from "./HeaderButtons";
-import UserIcon from "./UserIcon";
+import BurguerMenu from "../assets/burger-menu-svgrepo-com.svg";
 
 function Header() {
   return (
-    <header className="container-header">
-      <img src={Logo} alt="" />
-      <div className="buttons-box">
-        <HeaderButton ButtonIcon={HomeIcon} ButtonText="Home" />
-        <HeaderButton ButtonIcon={TicketIcon} ButtonText="Ingressos" />
-        <HeaderButton ButtonIcon={CardIcon} ButtonText="Minha carteirinha" />
-      </div>
-      <UserIcon />
-    </header>
+    <>
+      <header className="container-header">
+        <img src={Logo} alt="Logo" />
+
+        <div className="buttons-box">
+          <div className="desktop-buttons">
+            <HeaderButton ButtonIcon={HomeIcon} ButtonText="Home" />
+            <HeaderButton ButtonIcon={TicketIcon} ButtonText="Ingressos" />
+            <HeaderButton
+              ButtonIcon={CardIcon}
+              ButtonText="Minha carteirinha"
+            />
+          </div>
+
+          <div className="mobile-button">
+            <HeaderButton
+              ButtonIcon={BurguerMenu}
+              ButtonText="Menu"
+              Width={40}
+            />
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
 
