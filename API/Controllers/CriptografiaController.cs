e04df92daefeb7c60cecff6d350846f75b16aea1
marioprneto <netogurupi2@gmail.com>
2025-09-01 13:48:27 -0300

    Ajuste na estrutura da API

---
+++ API/Controllers/CriptografiaController.cs
@@ -0,0 +1,24 @@
+﻿using API.Criptografia;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Validator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CriptografiaController : ControllerBase
+    {
+        [HttpGet("criptografarSenha")]
+        public void CriptografarSenha([FromQuery] string texto)
+        {
+            try
+            {
+                var criptografia = new CriptografiaAES();
+                criptografia.CriptografarAES(texto);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
