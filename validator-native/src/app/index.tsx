8a6ce7f0055e8bda40ca921f305f026a8337d118
hugo vilar <hugovilar04@gmail.com>
2025-09-11 08:53:04 -0300

    refactor: retirando lógica relacionada a tratamento de diferentes componentes para o header

--- validator-native/src/app/index.tsx
+++ validator-native/src/app/index.tsx
@@ -1,13 +1,6 @@
-import { View } from "react-native";
-import Header from "@/components/Header/Header.web";
 import React from "react";
-import { Platform } from "react-native";
-
-// if (Platform.OS === "web") {
-//   module.exports = require("@/components/Header/Header.web").default;
-// } else {
-//   module.exports = require("@/components/Header/Header.native").default;
-// }
+import { View } from "react-native";
+import Header from "@/components/Header/Header";
 
 export default function Index() {
   return (
