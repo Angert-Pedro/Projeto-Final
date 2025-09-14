677ef0bb0b07b69961b16ea452673ab6cfa7721a
Mário Neto <73663266+marioprneto@users.noreply.github.com>
2025-09-14 19:47:21 -0300

    Update and rename Program.cs to Validator/Program.cs

--- Validator/Program.cs
+++ Validator/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//Injeção de dependências
+//InjeÃ§Ã£o de dependÃªncias
 builder.Services.AddScoped<DataContext>();
 builder.Services.AddScoped(typeof(DAL<>));
 builder.Services.AddScoped(typeof(BaseService<>));
