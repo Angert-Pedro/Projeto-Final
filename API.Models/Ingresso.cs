7905213935a1879064821045b68d97a4191e806b
Thiago Silva <86689822+Wooggi1@users.noreply.github.com>
2025-11-19 17:01:16 -0300

    fix: Logica de Ingresso criava um evento novo

--- API.Models/Ingresso.cs
+++ API.Models/Ingresso.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,11 @@ namespace API.Models
         public int Id { get; set; }
         public string Codigo { get; set; }
         public string Tipo { get; set; }
-        public virtual Evento Evento_ { get; set; }
+
+        [Column("evento_id")]
+        public int EventoId { get; set; }
+        public virtual Evento? Evento_ { get; set; }
+
         public DateTime Data_Compra { get; set; }
         public bool Valido { get; set; }
         public int Lote { get; set; }
